21b6347db99fed975ceb248a1bbccc880a210271
Silver Sten Kruus <silverkruus@gmail.com>
2018-12-03 08:21:37 +0200

    Initialize Project

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+let path = require('path');
+let webpack = require('webpack');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.js$/,
+                loader: 'babel-loader',
+                include: path.join(__dirname, 'app'),
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+
+      }
+    ]
+  }
+}
