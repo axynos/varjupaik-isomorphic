74e5c92d5955e4e988d08af4fe5fce92f6a94540
Silver Sten Kruus <silverkruus@gmail.com>
2018-12-29 00:52:52 +0200

    Add more page skeleton content.

---
+++ components/teenused/table.js
@@ -0,0 +1,5 @@
+export default () => (
+  <div>
+    This is a table.
+  </div>
+)
--- pages/teenused.js
+++ pages/teenused.js
@@ -1,7 +1,16 @@
 import Layout from '../components/layout/default'
+import Table from '../components/teenused/table'
+import Service from '../components/teenused/service'
 
 export default () => (
   <Layout>
-    {/* Content */}
+    <h1>Teenused</h1>
+    <p>Siin on meie teenuste nimekiri, allpool on saadaval teenuste pikemad
+      kirjeldused ning tingimused.
+    </p>
+
+    <Table />
+    <Service />
+
   </Layout>
 )
