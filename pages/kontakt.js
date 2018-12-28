ff3169403206d328d2cf9b822359c28954c69d9a
Silver Sten Kruus <silverkruus@gmail.com>
2018-12-29 00:40:33 +0200

    Add skeleton content for contact page.

--- pages/kontakt.js
+++ pages/kontakt.js
@@ -1,7 +1,35 @@
 import Layout from '../components/layout/default'
+import contactData from '../data/contacts.json'
 
 export default () => (
   <Layout>
-    {/* Content */}
+    <section>
+      <div>
+        <h1>Kontakt</h1>
+        <div>
+          <h2>Üldmeil</h2>
+            <p>{contactData.email}</p>
+          <h2>Juhataja <span>Viktor Karhu</span></h2>
+            <p>(+372) 736 1520</p>
+          <h2>Sotsiaaltöötajad</h2>
+            {contactData.phones.socialWorkers.map((item, index) =>
+              (<p key={index}>{item}</p>))
+            }
+          <h2>Tugiisikud</h2>
+            {contactData.phones.supportPersons.map((item, index) =>
+              (<p key={index}>{item}</p>))
+            }
+        </div>
+      </div>
+
+      <div>
+        <h2>Asukoht</h2>
+        <p>{contactData.location}</p>
+      </div>
+    </section>
+
+    <section>
+      Map Section
+    </section>
   </Layout>
 )
