0f3541910438b95419f8c7b281044540e647a2b4
Silver Sten Kruus <silverkruus@gmail.com>
2019-01-05 02:50:52 +0200

    Add styling for error page.

--- pages/_error.js
+++ pages/_error.js
@@ -1,17 +1,55 @@
 import React from 'react'
+import css from 'styled-jsx/css'
+
+const globalStyle = css.global`
+  body {
+    margin: 0;
+    padding: 0;
+  }
+
+  html {
+    margin: 0;
+    padding: 0;
+  }
+`
+
+const style = css`
+  main {
+    height: 98vh;
+    width: 98vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    overflow: hidden;
+  }
+
+  h1 {
+    font-family: 'Roboto', sans-serif;
+  }
+
+  span:first-of-type {
+    padding-right: 0.5em;
+    border-right: 3px solid black;
+  }
+
+  span:last-of-type {
+    padding-left: 0.5em;
+  }
+`
 
 const Error = ( {statusCode} ) => {
   const message = (statusCode) => {
     if (statusCode) {
       switch (true) {
         case (statusCode >= 500):
-          return `${statusCode} | Veebilehe serveris tekkis viga.`
+          return `Veebilehe serveris tekkis viga.`
           break;
         case (statusCode == 404):
-          return `${statusCode} | Seda lehte ei leitud.`
+          return `Seda lehte ei leitud.`
           break;
         default:
-          return `${statusCode} | Veebilehes tekkis viga.`
+          return `Veebilehes tekkis viga.`
           break;
       }
     } else {
@@ -20,9 +58,13 @@ const Error = ( {statusCode} ) => {
   }
 
   return (
-    <p>
-      {message(statusCode)}
-    </p>
+    <main>
+      <h1>
+        <span>{statusCode}</span><span>{message(statusCode)}</span>
+      </h1>
+      <style jsx>{style}</style>
+      <style jsx global>{globalStyle}</style>
+    </main>
   )
 }
 
